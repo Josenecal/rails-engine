7ad73dbb60ee9d8b9c71156ecac33ba1b7814da4
Joseph Senecal <jasenecal2@gmail.com>
2022-05-18 14:24:11 -0600

    Test: expand merchant show spec testing

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :merchant do
     name { Faker::Commerce.vendor }
     created_at { Faker::Date.between(from: 5.years.ago, to: Date.today) }
-    updated_at {  Faker::Date.between(from: 5.years.ago, to: Date.today) }
+    updated_at { Faker::Date.between(from: 5.years.ago, to: Date.today) }
   end
 end
--- spec/requests/api/v1/merchants/show_spec.rb
+++ spec/requests/api/v1/merchants/show_spec.rb
@@ -5,6 +5,23 @@ RSpec.describe "merchants index endpoint: /api/v1/merchants/:id" do
     merchant_1 = create(:merchant)
     get "/api/v1/merchants/#{merchant_1.id}"
     expect(response).to be_successful
-    JSON.parse response.body
+    JSON.parse response.body, symbolize_names: true
+  end
+
+  describe "response body shape" do
+    it "responds in JSON:API if found" do
+      merchant_1 = create(:merchant)
+      merchant_2 = create(:merchant)
+      get "/api/v1/merchants/#{merchant_1.id}"
+      response_body = JSON.parse(response.body, symbolize_names: true)
+      expect(response_body[:data].class).to eq Hash
+      expect(response_body[:data][:type]).to eq "merchant"
+      expect(response_body[:data][:id].class).to eq Integer
+      expect(response_body[:data][:attributes].class).to eq Hash
+      expect(response_body[:data][:attributes][:name].class).to eq String
+      expect(Time.parse(response_body[:data][:attributes][:created_at]).class).to eq Time
+      expect(Time.parse(response_body[:data][:attributes][:updated_at]).class).to eq Time
+    end
+
   end
 end
