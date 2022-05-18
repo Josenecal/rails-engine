d79da1add26117a7c1457987e61a1b071fac74b7
Joseph Senecal <jasenecal2@gmail.com>
2022-05-18 13:49:20 -0600

    Fix: call correct endpoint in show test

--- spec/requests/api/v1/merchants/show_spec.rb
+++ spec/requests/api/v1/merchants/show_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "merchants index endpoint: /api/v1/merchants/:id" do
   it 'responds with JSON' do
-    get '/api/v1/merchants'
+    merchant_1 = create(:merchant)
+    get "/api/v1/merchants/#{merchant_1.id}"
     expect(response).to be_successful
     JSON.parse response.body
   end
