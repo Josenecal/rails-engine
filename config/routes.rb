5cdbb575b780f2fc2c7af3e94f3f5733c6078c1a
Joseph Senecal <jasenecal2@gmail.com>
2022-05-18 13:59:52 -0600

    SetUp: add all merchants endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/api/v1/merchants', to: 'api/v1/merchants#index'
+  get 'api/v1/merchants', to: 'api/v1/merchants#index'
+  get 'api/v1/merchants/:id', to: 'api/v1/merchants#show'
+
+  get 'api/v1/merchants/:id/items', to: 'api/v1/merchant_items#index'
+
+  get 'api/v1/items', to: 'api/v1/items#index'
 end
