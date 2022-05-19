ba1268002725fa6a770df0bbe1b7f12d6e393d03
Joseph Senecal <jasenecal2@gmail.com>
2022-05-19 00:29:10 -0600

    Feat: items create and destroy actions

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -8,4 +8,25 @@ class Api::V1::ItemsController < ApplicationController
     render json: ItemSerializer.find_item(Item.find(params[:id]))
   end
 
+  def create
+    item = Item.create!(
+      name: item_params[:name],
+      description: item_params[:description],
+      unit_price: item_params[:unit_price],
+      merchant_id: item_params[:merchant_id]
+    )
+    render json: ItemSerializer.find_item(item), status: 201
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    Item.destroy(params[:id])
+    render json: ItemSerializer.find_item(item), status: 202
+  end
+
+private
+
+  def item_params
+    params.permit(:name, :description, :unit_price, :merchant_id)
+  end
 end
