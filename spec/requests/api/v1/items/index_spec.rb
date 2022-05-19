87ae16fd202b4977d18225f55333b9b1ae3d2f14
Joseph Senecal <jasenecal2@gmail.com>
2022-05-18 22:37:05 -0600

    Test: expand item index request tests

--- spec/requests/api/v1/items/index_spec.rb
+++ spec/requests/api/v1/items/index_spec.rb
@@ -9,16 +9,16 @@ RSpec.describe "items index endpoint: 'api/v1/items'" do
 
   describe "response shape" do
 
-    xit 'returns expected keys and value formats' do
-      create_list(:items, 5)
-      get 'api/v1/items'
+    it 'returns expected keys and value formats' do
+      create_list(:item, 5)
+      get '/api/v1/items'
       response_body = JSON.parse(response.body, symbolize_names: true)
 
       expect(response_body[:data].length).to eq 5
       expect(response_body[:data].class).to eq Array
       response_body[:data].each do |resource|
         expect(resource[:type]).to eq "item"
-        expect(resource[:id]).to match /\d+/ # Int regex
+        expect(resource[:id]).to match /\d+/ # Integer in regex
         expect(resource[:attributes].class).to eq Hash
         expect(resource[:attributes][:name].class).to eq String
         expect(resource[:attributes][:description].class).to eq String
@@ -26,6 +26,13 @@ RSpec.describe "items index endpoint: 'api/v1/items'" do
       end
     end
 
+    it "returns an empty array if no items are present" do
+      get '/api/v1/items'
+      response_body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response_body[:data].class).to eq Array
+      expect(response_body[:data].empty?).to eq true
+    end
   end
 
 end
