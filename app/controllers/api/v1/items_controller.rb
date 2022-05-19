bc05060f8dca744326441757018580bf38265a54
Joseph Senecal <jasenecal2@gmail.com>
2022-05-18 23:02:26 -0600

    Feat: show action for item controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::ItemsController < ApplicationController
+
   def index
     render json: ItemSerializer.all_items(Item.all)
   end
+
+  def show
+    render json: ItemSerializer.find_item(Item.find(params[:id]))
+  end
+
 end
