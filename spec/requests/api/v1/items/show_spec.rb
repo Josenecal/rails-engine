0efd4a2d9f5d0905e98935a4912b204f588e2527
Joseph Senecal <jasenecal2@gmail.com>
2022-05-18 23:00:38 -0600

    Test: item show response shape passing

--- spec/requests/api/v1/items/show_spec.rb
+++ spec/requests/api/v1/items/show_spec.rb
@@ -10,23 +10,22 @@ RSpec.describe "items show endpoint: 'api/v1/items/:id'" do
 
   describe "response shape" do
 
-    xit 'returns expected keys and value formats' do
+    it 'returns expected keys and value formats' do
       item_1 = create(:item)
       get "/api/v1/items/#{item_1.id}"
       response_body = JSON.parse(response.body, symbolize_names: true)
-
       expect(response_body[:data].class).to eq Hash
-        expect(resource[:type]).to eq "item"
-        expect(resource[:id]).to match /\d+/ # Integer in regex
-        expect(resource[:attributes].class).to eq Hash
-        expect(resource[:attributes][:name].class).to eq String
-        expect(resource[:attributes][:description].class).to eq String
-        expect(resource[:attributes][:unit_price].class).to eq Float
-      end
+      expect(response_body[:data][:type]).to eq "item"
+      expect(response_body[:data][:id]).to match /\d+/ # Integer in regex
+      expect(response_body[:data][:attributes].class).to eq Hash
+      expect(response_body[:data][:attributes][:name].class).to eq String
+      expect(response_body[:data][:attributes][:description].class).to eq String
+      expect(response_body[:data][:attributes][:unit_price].class).to eq Float
     end
 
     xit "returns 404 if bad merchant id provided" do
-      merchant_1 = create(merchant)
+      # Test failing - throws ActiveRecord::RecordNotFound:, implement rescue?
+      merchant_1 = create(:merchant)
       merchant_id = merchant_1.id
       Merchant.destroy(merchant_id)
       get "/api/v1/items/#{merchant_id}"
