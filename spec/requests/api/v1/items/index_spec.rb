cbf7d21d6ad240435f2bfe0675e7490a9c1790aa
Joseph Senecal <jasenecal2@gmail.com>
2022-05-18 22:20:02 -0600

    Fix: misplaced parenthesis and bad uri call

--- spec/requests/api/v1/items/index_spec.rb
+++ spec/requests/api/v1/items/index_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe "items index endpoint: '/items'" do
+RSpec.describe "items index endpoint: 'api/v1/items'" do
 
   it "returns a response" do
-    get '/items'
+    get '/api/v1/items'
     expect(response).to be_successful
   end
 
   describe "response shape" do
 
-    it 'returns expected keys and value formats' do
+    xit 'returns expected keys and value formats' do
       create_list(:items, 5)
-      get '/items'
-      response_body = JSON.parse(response.body), symbolize_names :true
+      get 'api/v1/items'
+      response_body = JSON.parse(response.body, symbolize_names: true)
 
       expect(response_body[:data].length).to eq 5
       expect(response_body[:data].class).to eq Array
