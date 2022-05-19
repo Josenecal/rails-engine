17c57c2febd92e69cded004de40e3631cb9990fe
Joseph Senecal <jasenecal2@gmail.com>
2022-05-18 23:01:50 -0600

    Feat: ItemSerializer::find_item(item)

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -17,4 +17,21 @@ class ItemSerializer
       end
     }
   end
+
+  def self.find_item(item)
+    {
+      data: {
+        type: "item",
+        id: item.id.to_s,
+        attributes: {
+          name: item.name,
+          description: item.description,
+          unit_price: item.unit_price,
+          merchant_id: item.merchant_id
+        }
+      }
+    }
+  end
+
+  
 end
