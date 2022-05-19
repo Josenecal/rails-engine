dbbf075cfff1dc6cd74f18df9b64571969a85856
Joseph Senecal <jasenecal2@gmail.com>
2022-05-18 22:37:29 -0600

    Feat: items index endpoint

---
+++ app/serializers/item_serializer.rb
@@ -0,0 +1,20 @@
+class ItemSerializer
+  # Refactor Opportunity - duplicated with merchant item serializer class
+  # Refactory Opportunity - also a valid spot to add a links element(element?)
+  def self.all_items(items)
+    {
+      data: items.map do |item|
+        {
+          type: "item",
+          id: item.id.to_s,
+          attributes: {
+            name: item.name,
+            description: item.description,
+            unit_price: item.unit_price,
+            merchant_id: item.merchant_id
+          }
+        }
+      end
+    }
+  end
+end
