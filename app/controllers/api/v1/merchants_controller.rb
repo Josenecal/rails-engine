c0fcfa6976f2ae29e9e5c959eb8ed3d3fe816fb1
Joseph Senecal <jasenecal2@gmail.com>
2022-05-19 18:03:33 -0600

    Feat: route and controller action for find_all merchants endpoint

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,8 +1,15 @@
 class Api::V1::MerchantsController < ApplicationController
+
   def index
     render json: MerchantSerializer.all_merchants(Merchant.all)
   end
+
   def show
     render json: MerchantSerializer.find_merchant(Merchant.find(params[:id]))
   end
+
+  def find_all
+    Merchant.find_all_by_name(params[:name])
+  end
+
 end
