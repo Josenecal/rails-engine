b4c0fe367fb1a1d902f6d25bb4d78136773d568b
Joseph Senecal <jasenecal2@gmail.com>
2022-05-19 23:03:29 -0600

    Test: bulk out request specs for item find endpoint

--- spec/requests/api/v1/items/find_spec.rb
+++ spec/requests/api/v1/items/find_spec.rb
@@ -25,4 +25,39 @@ RSpec.describe "find item by name endpoint: '/api/v1/items/find?name=query'" do
     expect(expected[:data][:attributes][:merchant_id]).to eq item2.merchant_id
   end
 
+  it "accepts price parameters instead of name parameters" do
+    item1 = create(:item, unit_price: 4.99, name: "aa")
+    item2 = create(:item, unit_price: 5.0, name: "c")
+    item3 = create(:item, unit_price: 10.0, name: "B")
+    get "/api/v1/items/find?min_price=5&max_price=10"
+binding.pry
+    response_body = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response_body[:data][:id].to_i).to eq(item3.id)
+    expect(response_body[:data][:type]).to eq("item")
+  end
+
+  it "accepts just a minimum or just a maximum price" do
+    item1 = create(:item, unit_price: 4.99, name: "aa")
+    item2 = create(:item, unit_price: 5, name: "c")
+    item3 = create(:item, unit_price: 10.0, name: "B")
+    get "/api/v1/items/find?min_price=5"
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response_body[:data][:id].to_i).to eq(item3.id)
+    expect(response_body[:data][:type]).to eq("item")
+
+    get "/api/v1/items/find?max_price=10"
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response_body[:data][:id].to_i).to eq(item1.id)
+    expect(response_body[:data][:type]).to eq("item")
+  end
+
+  it "returns 404 if no matching results found" do
+    get "/api/v1/items/find?max_price=10"
+  end
+
 end
