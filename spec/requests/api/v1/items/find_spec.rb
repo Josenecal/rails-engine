0b8d1ed51779ac00b2046f88a8b403199ced63cc
Joseph Senecal <jasenecal2@gmail.com>
2022-05-19 19:19:09 -0600

    Test: add test for find one item endpoint response shape and content

--- spec/requests/api/v1/items/find_spec.rb
+++ spec/requests/api/v1/items/find_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe "find item by name endpoint: '/api/v1/items/find?name=query'" do
     expect(response).to be_successful
   end
 
+  it "provides the correct content and shape" do
+    item1 = create(:item, name: "Dog And Cat Depot")
+    item2 = create(:item, name: "Catastrophie Pet Supply")
+    item3 = create(:item, name: "Vacation Alley")
+    item4 = create(:item, name: "All For The Dogs")
+    get '/api/v1/items/find?name=cat'
+    expected = JSON.parse(response.body, symbolize_names: true)
+
+    expect(expected.keys.length).to eq 1
+    expect(expected[:data].class).to eq Hash
+    expect(expected[:data][:type]).to eq "item"
+    expect(expected[:data][:id]).to eq item2.id.to_s
+  end
+
 end
