561e1500424d3653d199b92148594eeb24f7c3c0
Joseph Senecal <jasenecal2@gmail.com>
2022-05-19 18:03:57 -0600

    Test: model test for merchant find_all method

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -15,4 +15,19 @@ RSpec.describe Merchant, type: :model do
     it { should have_many :transactions }
 
   end
+
+  describe "methods" do
+
+    it "self.find_all_by_name returns all matching names" do
+      merchant1 = create(:merchant, name: "Ronald Regan")
+      merchant2 = create(:merchant, name: "Aaron Industrial")
+      merchant3 = create(:merchant, name: "Erin and Kelly Kitchens")
+      expected = Merchant.find_all_by_name("ron")
+
+      expect(expected.include?(merchant1)).to eq true
+      expect(expected.include?(merchant2)).to eq true
+      expect(expected.include?(merchant3)).to eq false
+    end
+
+  end
 end
