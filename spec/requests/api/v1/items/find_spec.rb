68a99c438a2f2a0a058b47cdc1de877ba1b1810e
Joseph Senecal <jasenecal2@gmail.com>
2022-05-19 19:22:09 -0600

    Ref: bulk up response shape test assertions

--- spec/requests/api/v1/items/find_spec.rb
+++ spec/requests/api/v1/items/find_spec.rb
@@ -19,6 +19,10 @@ RSpec.describe "find item by name endpoint: '/api/v1/items/find?name=query'" do
     expect(expected[:data].class).to eq Hash
     expect(expected[:data][:type]).to eq "item"
     expect(expected[:data][:id]).to eq item2.id.to_s
+    expect(expected[:data][:attributes][:name]).to eq item2.name
+    expect(expected[:data][:attributes][:description]).to eq item2.description
+    expect(expected[:data][:attributes][:unit_price]).to eq item2.unit_price
+    expect(expected[:data][:attributes][:merchant_id]).to eq item2.merchant_id
   end
 
 end
